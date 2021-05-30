95c9807e438abbfa2a9e09a9f7d634d988004721
Jolud95 <www.appel@mail.ru>
2021-05-30 14:39:54 +0300

    Redux-form

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,6 +1,46 @@
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
+import React from "react";
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+
 const Login = () => {
+    const onSubmit = (formData:FormDataType) => {
+        console.log(formData)
+    }
     return (
-        <h1>LOGIN</h1>
+        <div>
+            <h1>LOGIN</h1>
+            <ReduxLoginForm onSubmit={onSubmit}/>
+        </div>
     )
 }
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <div>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={"Login"} name={"login"} component={"input"}/>
+                </div>
+                <div>
+                    <Field placeholder={"Password"} name={"password"} component={"input"}/>
+                </div>
+                <div>
+                    <Field type={"checkbox"} name={"rememberMe"} component={"input"}/> remember me
+                </div>
+                <div>
+                    <button>LogIn</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const ReduxLoginForm = reduxForm<FormDataType>({form: "login"})(LoginForm)
+
 export default Login;
